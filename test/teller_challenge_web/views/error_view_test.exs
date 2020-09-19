3aa2fde81f0ec84c38a9433e18f2f27dc01e9059
Daniel Khaapamyaki <dasky@hey.com>
2020-09-19 19:24:03 +0200

    update error view test to json

--- test/teller_challenge_web/views/error_view_test.exs
+++ test/teller_challenge_web/views/error_view_test.exs
@@ -4,11 +4,14 @@ defmodule TellerChallengeWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(TellerChallengeWeb.ErrorView, "404.html", []) == "Not Found"
+  test "renders 404.json" do
+    assert render(TellerChallengeWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
-  test "renders 500.html" do
-    assert render_to_string(TellerChallengeWeb.ErrorView, "500.html", []) == "Internal Server Error"
+  test "renders 500.json" do
+    assert render(TellerChallengeWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
   end
 end
