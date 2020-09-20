d4d890e30e513d05a5f82dd1108b79e730bdb75e
Daniel Khaapamyaki <dasky@hey.com>
2020-09-20 18:23:46 +0200

    improve metrics test

--- test/teller_challenge_web/metrics_test.exs
+++ test/teller_challenge_web/metrics_test.exs
@@ -6,12 +6,13 @@ defmodule TellerChallengeWeb.MetricsTest do
   setup do
     timestamp = :os.system_time(:seconds)
     requests = for offset <- 1..5, do: %{timestamp: timestamp - offset, path: "/accounts"}
-    [requests: requests]
+    [requests: requests, timestamp: timestamp]
   end
 
   test "handle_cast({:push, path}), state) returns last 5 seconds worth of requests", context do
     {:noreply, state} = Metrics.handle_cast({:push, "/accounts"}, context.requests)
 
     assert Enum.count(state) == 5
+    assert state == for offset <- 0..4, do: %{path: "/accounts", timestamp: context.timestamp - offset}
   end
 end
